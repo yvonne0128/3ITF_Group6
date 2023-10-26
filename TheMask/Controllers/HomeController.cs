554362943047485114bb8ce58122ee9ace174959
Ryan Dy <dy.ryan.christopher@gmail.com>
2023-10-26 09:38:58 +0800

    Add project files.

---
+++ TheMask/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TheMask.Models;
+
+namespace TheMask.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Makeup()
+        {
+            return View();
+        }
+
+        public IActionResult Skincare()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+       public IActionResult Login()
+       {
+            return View();
+       }
+
+       public IActionResult Signup()
+       {
+           return View();
+       }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+} 
